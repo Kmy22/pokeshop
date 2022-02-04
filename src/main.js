890d215926f7d97e852e984576a9c5513c3fb3b2
Kmy22 <camillelp13@gmail.com>
2022-02-04 15:19:51 +0100

    oui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Primevue from 'primevue/config'
+import Button from 'primevue/button'
+import Inputtext from 'primevue/inputtext'
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+const app = createApp(App);
+app.use(Primevue);
+
+app.component('Button',Button);
+app.component('Inputtext',Inputtext);
+
+app.mount('#app');
