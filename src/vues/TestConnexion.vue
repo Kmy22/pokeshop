4c0cd8cc3fecb3ab9df326d49acb6e4248de63e8
Kmy22 <camillelp13@gmail.com>
2022-02-04 17:23:57 +0100

    oups un oubli

---
+++ src/vues/TestConnexion.vue
@@ -0,0 +1,68 @@
+<template>
+ <div>
+   <h1> ---- TEST DE CONNEXION ---- </h1>
+   User  :<Inputtext v-model="text_user" />
+   Pwd :<Inputtext v-model="text_pwd" />
+   <Button label="Connexion" icon = "pi pi-user" @click="connexion"></Button>  
+   <p>
+    DEBUG inputs : {{text_user}}  | {{text_pwd}}
+   </p>
+    
+   <Toast />
+ </div>
+</template>
+
+<script>
+
+import { defineComponent, ref } from "vue";
+import {useToast} from 'primevue/usetoast';
+
+export default defineComponent({
+  setup() {
+      const toast = useToast();
+      const text_user = ref("user name") ;  
+      const text_pwd = ref("password");      
+
+    const connexion = () => {
+      toast.add({sererity: 'info', summary: 'Connexion', detail: "user :" + text_user.value + "pwd : " + text_pwd.value });
+      console.log(text_user.value + " " + text_pwd.value);
+    }
+
+
+      return{
+        text_user, 
+        text_pwd,     
+        connexion
+      }
+  }
+})
+
+// 2 methodes différents ! 
+/*
+export default {
+  data(){
+    return{
+      text_user : "user name",    
+      text_pwd : "password",
+    }     
+  } ,
+  
+  methods:{
+    connexion(){
+      this.$toast.add({sererity: 'info', summary: 'Connexion', detail: this.text_user});
+    }
+  }
+}
+*/
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/vues/TestNotes.vue
@@ -0,0 +1,45 @@
+<template>
+ <div>
+    <h1> ---- TEST NOTATION ---- </h1>
+   <Rating v-model="val" /> 
+   <Button label="Noter" icon = "pi pi-user" @click="note"></Button>  
+   <p> 
+    DEBUG note : {{val}} 
+    </p>
+   <Toast />
+ </div>
+</template>
+
+<script>
+
+import { defineComponent, ref } from "vue";
+import {useToast} from 'primevue/usetoast';
+
+export default defineComponent({
+  setup() {
+      const toast = useToast();     
+      const val = ref(3);
+
+    const note = () => {
+      toast.add({sererity: 'info', summary: 'note', detail: val});
+    }
+
+      return{      
+        val,
+        note
+      }
+  }
+})
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
